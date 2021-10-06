76dbc759fff3a3d5063dc57490a1d567b4281924
Pavel <myagkiypavel@gmail.com>
2021-10-06 16:51:15 +0300

    Refactor: Set array to children

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,17 @@ function App() {
   const date = newDate.getDate();
   const month = newDate.getMonth();
   const year = newDate.getFullYear(); 
+  const msg = "Hello"
   
   const el = React.createElement('div', { style: { color: 'red' } }, `${date}/${month}/${year}`)
   const el1 = React.createElement('p', { id: 2, }, 'Element: p')
+  const h2 = <h2 children={['Hello1', 'Hello2']}></h2>
   return (
     <div className="App">
       <h1>Hello</h1>
       {el}
       {el1}
+      {h2}
     </div>
   );
 }
