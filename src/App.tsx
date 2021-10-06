37cd890342d14d857dba07454993fa03ddfb7922
Pavel <myagkiypavel@gmail.com>
2021-10-06 16:26:46 +0300

    Fix: Show Date

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const el = React.createElement('div', {style:{color: 'red'}}, 'Element: div')
-  const el1 = React.createElement('p', {id: 2,}, 'Element: p')
+  const newDate = new Date()
+  const date = newDate.getDate();
+  const month = newDate.getMonth();
+  const year = newDate.getFullYear(); 
+  
+  const el = React.createElement('div', { style: { color: 'red' } }, `${date}/${month}/${year}`)
+  const el1 = React.createElement('p', { id: 2, }, 'Element: p')
   return (
     <div className="App">
       <h1>Hello</h1>
