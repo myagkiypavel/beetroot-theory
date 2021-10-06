606041a85a950cc3bb91061c4870aecb08b639ef
Pavel <myagkiypavel@gmail.com>
2021-10-06 17:16:44 +0300

    Chore: create example components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface Props {
+  msg: string;
+}
+
+interface PropsType {
+  children: {}
+}
+
+interface ChildrenProps{
+  children: string;
+  color: string;
+}
+
 function App() {
+  // const message = (props: Props) => <h1>{props.msg}</h1>
+  // const Message = (props: Props) => <h1>{props.msg}</h1>
+  const Message = ({children, color}: ChildrenProps) => <h1 style={{color}}>{children}</h1>
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {/* {message({msg: 'Hello React'})}
+      {message({msg: 'Hello World'})} */}
+
+      {/* <Message msg='Hello React'/>
+      <Message msg='Hello World'/> */}
+
+      <Message color='red'>Hello React</Message>
+      <Message color='green'>Hello World</Message>
     </div>
   );
 }
