1a602fb13f94868f4275f2b44b880ae3cff9dbf9
Pavel <myagkiypavel@gmail.com>
2021-10-06 18:10:06 +0300

    Chore: Implement Style

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface Props{
+  style?: {};
+  className?: string;
+}
+
+function Box ({style, className = '', ...rest}: Props){
+  return (
+    <div
+      className={`box ${className}`.trim()} 
+      style={{border: '3px solid tomato', ...style}}
+      {...rest}
+    >
+      Box
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Box className='box__green' style={{border: '3px dashed black'}}/>
+      <Box/>
+      <Box/>
     </div>
   );
 }
