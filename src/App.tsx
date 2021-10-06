ae2a5e9728a5f3dea5d1e2cd428010bfa5b75565
Pavel <myagkiypavel@gmail.com>
2021-10-06 16:21:57 +0300

    My solution

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const el = React.createElement('div', {style:{color: 'red'}}, 'Element: div')
+  const el1 = React.createElement('p', {id: 2,}, 'Element: p')
   return (
     <div className="App">
       <h1>Hello</h1>
+      {el}
+      {el1}
     </div>
   );
 }
